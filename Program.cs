528354bf81250d65eec6e5ad7e2632a5e5c426d4
Miqueas Castañón <castmicah@gmail.com>
2026-02-24 23:51:13 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("¿Cuántos números desea ingresar? ");
+int n = int.Parse(Console.ReadLine());
+
+int numero;
+int positivos = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese un número: ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        positivos++;
+    }
+}
+
+Console.WriteLine("Cantidad de números positivos: " + positivos);
